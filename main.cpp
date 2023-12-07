9ac676a0bcbe2dc3d7ad82bf87fe70f1eda3593d
Ismailium <qiranorane55@gmail.com>
2023-12-07 19:59:17 +0100

    Added some functionalites | Edit on main functions :rocket:

--- main.cpp
+++ main.cpp
@@ -3,69 +3,86 @@
 
 using namespace std;
 
+// Books
+string Books[] = {
+        "Zickola's Land",
+        "AMARITA - Zickola's Land Part 2",
+        "The Valley Of The Forgotten wolves - Zickola's Land Part 3"
+};
+int BooksListSize = sizeof(Books)/sizeof(Books[0]);
+
+// Prices
+int Prices[] = {30, 25, 45};
+
+// Cart
+string Cart[] = {};
+int CartItemsLength = sizeof(Cart)/sizeof(Cart[0]);
+
+// Balance
+int Balance = 200;
+
 // Library Class
 class Library {
 public:
     // Show Books Function
-    static void showBooks(string *Products, int ProductsListSize, int *Prices) {
+    static void showBooks() {
         cout << "\n-- List Of The Available Books : --" << endl;
-        for (int index = 0; index < ProductsListSize; index++) {
-            cout << "(" << index+1 << ") " << Prices[index] << "Dh - " << Products[index] << endl;
+        for (int index = 0; index < BooksListSize; index++) {
+            cout << "(" << index+1 << ") " << Prices[index] << "Dh - " << Books[index] << endl;
         }
     }
 
     // Show User Balance Function
-    static void showBalance(int UserBalance) {
+    static void showBalance() {
         cout << "\n-- Your Balance : --" << endl;
-        cout << UserBalance << "Dh" << endl;
+        cout << Balance << "Dh" << endl;
     }
 
     // Show User Cart Function
-    static void showCart(string *userCart, int CartIListSize) {
-        bool isEmpty = CartIListSize == 0; // CartIListSize == 0 => return true else return false
+    static void showCart() {
+        bool isEmpty = CartItemsLength == 0; // CartIListSize == 0 => return true else return false
         cout << "\n-- Your Cart : --" << endl;
 
         if (!isEmpty) {
-            for (int index = 0; index < CartIListSize; index++) {
-                cout << "- " << userCart[index] << endl;
+            for (int index = 0; index < CartItemsLength; index++) {
+                cout << "- " << Cart[index] << endl;
             }
         }
         else cout << "[]" << endl;
     }
 
     // Update The User Balance Function
-    static void updateUserBalance(int userBalance, int productPrice) {
-        int updatedBalance = userBalance - productPrice;
-        showBalance(updatedBalance);
+    static void updateUserBalance(int bookPrice) {
+        Balance = Balance - bookPrice;
+        showBalance();
     };
 
     // Update The User Cart Function
-    static void updateUserCart(string *userCart, const string &product, int CartIListSize) {
-        userCart->append(product);
-        showCart(userCart, CartIListSize);
+    static void updateUserCart(string book) {
+        Cart->append(book);
+        showCart();
     }
-    // Update User Cart Function Isn't Working The Way It Should :)
 
     // Buy Book Function
-    static void buyBook(string *Products, int productPosition, int ProductsListSize, const int *Prices, int userBalance, string  *userCart, int CartIListSize) {
+    static void buyBook(int bookPosition) {
         cout << "\n-----" << endl;
-        for (int index = 0; index < ProductsListSize; index++) {
-            if (productPosition - 1 == index) {
+        for (int index = 0; index < BooksListSize; index++) {
+            if (bookPosition - 1 == index) {
 
-                const string selectedProductTitle = Products[productPosition - 1];
-                const int selectedProductPrice = Prices[productPosition - 1];
+                const string selectedBookTitle = Books[bookPosition - 1];
+                const int selectedProductPrice = Prices[bookPosition - 1];
 
-                cout << "Book: " << selectedProductTitle << endl;
+                cout << "Book: " << selectedBookTitle << endl;
                 cout << "Price: " << selectedProductPrice << "Dh" << endl;
-                updateUserBalance(userBalance,  selectedProductPrice);
-                updateUserCart(userCart, selectedProductTitle, CartIListSize);
+                updateUserBalance(selectedProductPrice);
+                updateUserCart(selectedBookTitle);
             }
         }
         cout << "-----" << endl;
     }
 
     // Ask To Buy Function
-    static int askToBuy(string *Products, int ProductsListSize, int *Prices, int userBalance, string *userCart, int CartIListSize) {
+    static int askToBuy() {
         int bookPosition;
         cout << "\nType the position of a book :";
         cin >> bookPosition;
@@ -73,7 +90,7 @@ public:
         // Aak For Confirmation Before Buying
         string confirmation = confirmBuy();
         if (confirmation == "y" || confirmation == "Y") {
-            buyBook(Products, bookPosition, ProductsListSize, Prices, userBalance, userCart, CartIListSize);
+            buyBook(bookPosition);
         }
         else return 0;
     }
@@ -87,32 +104,12 @@ public:
     }
 };
 
-
-// Books
-string Books[] = {
-        "Zickola's Land",
-        "AMARITA - Zickola's Land Part 2",
-        "The Valley Of The Forgotten wolves - Zickola's Land Part 3"
-};
-
-int BooksListSize = sizeof(Books)/sizeof(Books[0]);
-
-// Prices
-int Prices[] = {30, 25, 45};
-
-// Cart
-string Cart[] = {};
-int CartItemsLength = sizeof(Cart)/sizeof(Cart[0]);
-
-// Balance
-int Balance = 1000;
-
 int main() {
     // Call Library Functions
-    Library::showBooks(Books, BooksListSize, Prices);
-    Library::showBalance(Balance);
-    Library::showCart(Cart, CartItemsLength);
-    Library::askToBuy(Books, BooksListSize, Prices, Balance, Cart, CartItemsLength);
+    Library::showBooks();
+    Library::showBalance();
+    Library::showCart();
+    Library::askToBuy();
 
     return 0;
 }
